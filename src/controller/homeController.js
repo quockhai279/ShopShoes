b6c93119efa1509d7c8ad82a8ad6a1ca610d0d7b
Chau Quoc Khai <Khai.cq.27@gmail.com>
2023-04-07 05:39:00 +0700

    edit crud user

--- src/controller/homeController.js
+++ src/controller/homeController.js
@@ -13,15 +13,47 @@ let getHomepage = async (req, res) => {
         console.log(e)
     }
 }
+
 let getCRUD = (req, res) => {
     return res.render('crud.ejs')
 }
+
 let postCRUD = async (req, res) => {
     let message = await CRUDService.createNewUser(req.body)
     console.log(message)
     return res.send('hello')
 }
 
+let displayGetCRUD = async (req, res) => {
+    let data = await CRUDService.getAllUser()
+    console.log(data)
+    return res.render('displayCRUD.ejs', {
+        dataTable: data
+    })
+}
+
+let getEditCRUD = async (req, res) => {
+    let userId = req.params.id
+    if (userId) {
+        let userData = await CRUDService.getUserInfoById(userId)
+        return res.render('editCRUD.ejs', {
+            user: userData
+        })
+    }
+    else {
+        return res.send('User not found')
+    }
+}
+
+let putCRUD = async (req, res) => {
+    let data = req.body
+    let allUsers = await CRUDService.updateUserData(data)
+    return res.render('displayCRUD.ejs', {
+        dataTable: allUsers
+    })
+
+}
+
 module.exports = {
-    getHomepage, getCRUD, postCRUD
+    getEditCRUD, getHomepage, getCRUD, postCRUD, displayGetCRUD, putCRUD
 }
--- src/route/web.js
+++ src/route/web.js
@@ -7,6 +7,10 @@ const initWebRoute = (app) => {
     router.get('/', homeController.getHomepage);
     router.get('/crud', homeController.getCRUD);
     router.post('/post-crud', homeController.postCRUD);
+    router.get('/get-crud', homeController.displayGetCRUD);
+    router.get('/edit-crud/:id', homeController.getEditCRUD);
+    router.post('/put-crud', homeController.putCRUD);
+
 
     router.get('/about', (req, res) => {
         res.send('Hello World!')
