c9f0130dfa7d7d114da3a7cd01408d313b86c4ca
Chau Quoc Khai <Khai.cq.27@gmail.com>
2023-04-28 13:31:45 +0700

    edit userService & migration create user

--- src/services/userService.js
+++ src/services/userService.js
@@ -116,8 +116,9 @@ let createNewUser = (data) => {
                     lastName: data.lastName,
                     address: data.address,
                     phoneNumber: data.phoneNumber,
-                    gender: data.gender === '1' ? true : false,
-                    roleId: data.roleId
+                    gender: data.gender,
+                    roleId: data.roleId,
+                    positionId: data.positionId
                 })
                 resolve({
                     errCode: 0,
