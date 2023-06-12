2c649b79890c87c6a6be58ac87ed6ca1bcd54c58
Chau Quoc Khai <Khai.cq.27@gmail.com>
2023-06-12 07:08:38 +0700

    video 80

--- src/models/allcode.js
+++ src/models/allcode.js
@@ -13,6 +13,10 @@ module.exports = (sequelize, DataTypes) => {
             Allcode.hasMany(models.User, { foreignKey: 'positionId', as: 'positionData' })
             Allcode.hasMany(models.User, { foreignKey: 'gender', as: 'genderData' })
             Allcode.hasMany(models.Schedule, { foreignKey: 'timeType', as: 'timeTypeData' })
+
+            Allcode.hasMany(models.DoctorInfo, { foreignKey: 'priceId', as: 'priceTypeData' })
+            Allcode.hasMany(models.DoctorInfo, { foreignKey: 'provinceId', as: 'provinceTypeData' })
+            Allcode.hasMany(models.DoctorInfo, { foreignKey: 'paymentId', as: 'paymentTypeData' })
         }
     };
     Allcode.init({
--- src/models/doctor_info.js
+++ src/models/doctor_info.js
@@ -10,6 +10,12 @@ module.exports = (sequelize, DataTypes) => {
          * The `models/index` file will call this method automatically.
          */
         static associate(models) {
+            DoctorInfo.belongsTo(models.User, { foreignKey: 'doctorId' })
+
+            DoctorInfo.belongsTo(models.Allcode, { foreignKey: 'priceId', targetKey: 'keyMap', as: 'priceTypeData' })
+            DoctorInfo.belongsTo(models.Allcode, { foreignKey: 'provinceId', targetKey: 'keyMap', as: 'provinceTypeData' })
+            DoctorInfo.belongsTo(models.Allcode, { foreignKey: 'paymentId', targetKey: 'keyMap', as: 'paymentTypeData' })
+
         }
     };
     DoctorInfo.init({
